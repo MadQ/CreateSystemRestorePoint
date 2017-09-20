82bf37781d28ffd72245e98306e0a46af2f8ea41
MadQ <quinten.martens@jonasfitness.com>
2017-09-20 08:37:04 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateSystemRestorePoint
+{
+	class Program
+	{
+		static int Main(string[] args)
+		{
+			var restorePointName   = args.Length < 1 ? CreateNewName() : string.Join(" ", args);
+			var managementPath     = new ManagementPath(@"\\.\ROOT\DEFAULT:SystemRestore");
+			var systemRestoreClass = new ManagementClass(managementPath);
+			var methodParameters   = systemRestoreClass.Methods["CreateRestorePoint"].InParameters;
+			
+			methodParameters.Properties["Description"].Value = "Testing";
+			methodParameters.Properties["EventType"].Value   = 100u;
+			methodParameters.Properties["Description"].Value = 16u;
+			
+			var outParameters = systemRestoreClass.InvokeMethod("CreateRestorePoint", methodParameters, null);
+			var hresult       = unchecked( (int) (uint) outParameters["ReturnValue"]);
+			
+			// I suppose you could do this, but I'm not sure if it's necessary.
+			// Marshal.ThrowExceptionForHR(hresult);
+			
+			return hresult;
+		}
+		
+		
+		private static string CreateNewName()
+		{
+			var d = DateTime.Now;
+			
+			return $"RP_{d:yy-MM-dd_HH:mm:ss.fff}";
+		}
+	}
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CreateSystemRestorePoint")]
+[assembly: AssemblyDescription("Creates a System Restore Point")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("MadQ")]
+[assembly: AssemblyProduct("CreateSystemRestorePoint")]
+[assembly: AssemblyCopyright("Copyright © 2017 MadQ")]
+[assembly: AssemblyTrademark("MadQ™")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d65406eb-b684-473c-981c-4481688d59e3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.6.1.8")]
+[assembly: AssemblyFileVersion("1.6.1.8")]
+[assembly: NeutralResourcesLanguage("en-US")]
+
