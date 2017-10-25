1b26991b4decfd3c10a3a8486ae15c633312157a
MadQ <quinten.martens@jonasfitness.com>
2017-10-25 06:49:22 -0400

    Fixed a bug. Added help.

--- Program.cs
+++ Program.cs
@@ -1,25 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Management;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace CreateSystemRestorePoint
 {
 	class Program
 	{
+		[STAThread]
 		static int Main(string[] args)
 		{
+			if(HelpShown(args))
+				return 0;
+			
 			var restorePointName   = args.Length < 1 ? CreateNewName() : string.Join(" ", args);
 			var managementPath     = new ManagementPath(@"\\.\ROOT\DEFAULT:SystemRestore");
 			var systemRestoreClass = new ManagementClass(managementPath);
 			var methodParameters   = systemRestoreClass.Methods["CreateRestorePoint"].InParameters;
 			
-			methodParameters.Properties["Description"].Value = "Testing";
-			methodParameters.Properties["EventType"].Value   = 100u;
-			methodParameters.Properties["Description"].Value = 16u;
+			methodParameters.Properties["Description"].Value      = restorePointName;
+			methodParameters.Properties["EventType"].Value        = 100u;
+			methodParameters.Properties["RestorePointType"].Value = 16u;
 			
 			var outParameters = systemRestoreClass.InvokeMethod("CreateRestorePoint", methodParameters, null);
 			var hresult       = unchecked( (int) (uint) outParameters["ReturnValue"]);
@@ -30,12 +37,30 @@ namespace CreateSystemRestorePoint
 			return hresult;
 		}
 		
+		private static bool HelpShown(string[] args)
+		{
+			if(args.Length < 1 || !args[0].Contains("?"))
+				return false;
+			
+			MessageBox.Show($@"Creates a System Restore Point
+
+usage: {Path.GetFileName(typeof(Program).Assembly.Location)} [<Restore Point Name>]
+
+The default <Restore Point Name> is ""RP_yy-MM-dd_HH:mm:ss.fff"".
+
+Got it?"
+	, typeof(Program).Namespace, MessageBoxButtons.YesNo, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1);
+			
+			return true;
+		}
 		
 		private static string CreateNewName()
 		{
-			var d = DateTime.Now;
+			var ss = "yy-MM-dd_HH:mm:ss.fff";
 			
-			return $"RP_{d:yy-MM-dd_HH:mm:ss.fff}";
+			Console.WriteLine(ss);
+			
+			return $"RP_{DateTime.Now:yy-MM-dd_HH:mm:ss.fff}";
 		}
 	}
 }
